44d4d9f286e45eb779e0509584914140bd3d8413
Kevin Zhu <aikevinzhu@gmail.com>
2018-03-14 02:30:25 -0400

    Addded functions to get win rates for individual champs and matchups

--- attempt2.py
+++ attempt2.py
@@ -1,9 +1,38 @@
 import requests
 
-payload = {"api_key":"30ebfcd2e818fd77f872aa2fa8c5d068"}
-r = requests.get("http://api.champion.gg/stats", params = payload)
-page = r.json()
-champ = "Ezreal"
-champData = requests.get("http://api.champion.gg/stats/champs/" + champ, params = payload)
-page = champData.json()
-print(page)
+def getWinRate(champ, role):
+    payload = {"api_key":"30ebfcd2e818fd77f872aa2fa8c5d068"}
+    r = requests.get("http://api.champion.gg/stats/champs/" + champ, params = payload)
+    page = r.json()
+    print(page)
+    length = len(page)
+    winRate = 0
+    for i in range(length):
+        isRole = page[i].get("Role", default = "None")
+        if (role == isRole):
+            return page[i]["general"]["winPercent"]
+        
+        winRate = winRate + page[i]["general"]["winPercent"]
+    
+    return winRate / length
+
+def getMatchupWinRate(champ, opponent, role):
+    payload = {"api_key":"30ebfcd2e818fd77f872aa2fa8c5d068"}
+    r = requests.get("http://api.champion.gg/champion/" + champ + "/matchup/" + opponent, params = payload)
+    page = r.json()
+    if ("error" in page):
+        return 50.00
+    
+    print(page)
+    length = len(page)
+    for i in range(length):
+        isRole = page[i].get("role", default = "None")
+        if (role == isRole):
+            return page[i]["general"]["winRate"]
+
+    return 50.00
+
+
+
+print(getMatchupWinRate("Annie", "Aatrox", "Middle"))
+
